19bf0fe915930fbf6cfc8ed31135277052f37bf8
Nuno Pulido <junior.pulido.27@unsch.edu.pe>
2026-04-01 11:51:47 -0500

    actualizando apijs

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,5 +1,6 @@
 const BACKEND_URL =
-  import.meta.env.VITE_BACKEND_URL ||
-  (import.meta.env.DEV ? "http://localhost:9000" : "/api");
+  import.meta.env.DEV
+    ? "http://localhost:9000"
+    : "/api";
 
 export { BACKEND_URL };
