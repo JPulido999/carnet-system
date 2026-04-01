7347f9a690086d95726242386516f74950a882a3
Nuno Pulido <junior.pulido.27@unsch.edu.pe>
2026-04-01 11:42:55 -0500

    actualizando api js metodo de url para backend

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,1 +1,5 @@
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+const BACKEND_URL =
+  import.meta.env.VITE_BACKEND_URL ||
+  (import.meta.env.DEV ? "http://localhost:9000" : "/api");
+
+export { BACKEND_URL };
